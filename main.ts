3b3de178e1a289a52d183ee80f052bfea6f755c2
sarmpoy <118840155+sarmpoy@users.noreply.github.com>
2023-05-22 18:46:47 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
 basic.forever(function () {
-	
+    if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+        cuteBot.motors(50, 10)
+    } else {
+        cuteBot.motors(50, 50)
+    }
+})
+basic.forever(function () {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+        cuteBot.motors(10, 50)
+    } else {
+        cuteBot.motors(50, 50)
+    }
+})
+basic.forever(function () {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+        cuteBot.stopcar()
+    }
 })
